48361c3405cbd6b6453567728a2a9da478e84155
Artem <aciumac@vertafore.com>
2018-05-16 13:58:29 -0700

    Added database calls and everything works

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,36 @@
+# import pymysql
+import pyodbc
+
+
 def create_database(database_path: str):
-    #TODO: generate the database
-    pass
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=WordsDB;'
+                          'Trusted_Connection=Yes')
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if exists words")
+        ddl = "create table words(word varchar(25) primary key not null, usage_count int not null default 1);"
+        cur.execute(ddl)
+
+    print(database_path)
+    conn.close()
+
 
 def save_words_to_database(database_path: str, words_list: list):
-    #TODO: save the words to the database
-    pass
+    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=WordsDB;'
+                          'Trusted_Connection=Yes')
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            # check if word is in there #
+            sql = "select count(word) from words where word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word='" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
+            cur.execute(sql)
+        print("Database save complete!")
+
+    print(database_path)
+    conn.close()
